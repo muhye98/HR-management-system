40d4c37a61d5f411784fec7944f4bc63a6f0b4d9
muhye98 <mohee_ayeshs@yahoo.com>
2023-05-23 11:19:59 +0000

    fix the rander method

--- app.js
+++ app.js
@@ -49,22 +49,38 @@ let employee4 = new Employee("Omar Zaid", "Development", "Senior");
 let employee5 = new Employee("Rana Saleh", "Development	", "Junior");
 let employee6 = new Employee("Hadi Ahmad", "Finance	", "Mid-Senior");
 
+Employee.prototype.randerEmployee = function () {
+    document.write(`<p> Employee name: ${this.name} </p>`);
+    document.write(`<p> Employee name: ${this.dept} </p>`);
+    document.write(`<p> Employee name: ${this.salary} </p>`);
+    document.write(`</br>`);
+}
 
 
-document.write(`<p> Employee name: ${employee0.name} </p>   <p>Department: ${employee0.dept} </p>  <p>Employee salary: ${employee0.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee1.name} </p>   <p>Department: ${employee1.dept} </p>  <p>Employee salary: ${employee1.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee2.name} </p>   <p>Department: ${employee2.dept} </p>  <p>Employee salary: ${employee2.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee3.name} </p>   <p>Department: ${employee3.dept} </p>  <p>Employee salary: ${employee3.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee4.name} </p>   <p>Department: ${employee4.dept} </p>  <p>Employee salary: ${employee4.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee5.name} </p>   <p>Department: ${employee5.dept} </p>  <p>Employee salary: ${employee5.salary} </p>  `);
-document.write(`</br>`);
-document.write(`<p> Employee name: ${employee6.name} </p>   <p>Department: ${employee6.dept} </p>  <p>Employee salary: ${employee6.salary} </p>  `);
-document.write(`</br>`);
+employee0.randerEmployee();
+employee1.randerEmployee();
+employee2.randerEmployee();
+employee3.randerEmployee();
+employee4.randerEmployee();
+employee5.randerEmployee();
+employee6.randerEmployee();
+
+
+
+// document.write(`<p> Employee name: ${employee0.name} </p>   <p>Department: ${employee0.dept} </p>  <p>Employee salary: ${employee0.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee1.name} </p>   <p>Department: ${employee1.dept} </p>  <p>Employee salary: ${employee1.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee2.name} </p>   <p>Department: ${employee2.dept} </p>  <p>Employee salary: ${employee2.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee3.name} </p>   <p>Department: ${employee3.dept} </p>  <p>Employee salary: ${employee3.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee4.name} </p>   <p>Department: ${employee4.dept} </p>  <p>Employee salary: ${employee4.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee5.name} </p>   <p>Department: ${employee5.dept} </p>  <p>Employee salary: ${employee5.salary} </p>  `);
+// document.write(`</br>`);
+// document.write(`<p> Employee name: ${employee6.name} </p>   <p>Department: ${employee6.dept} </p>  <p>Employee salary: ${employee6.salary} </p>  `);
+// document.write(`</br>`);
 
 
 
